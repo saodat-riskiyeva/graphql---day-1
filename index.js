c6add807b603c3f1d312403120c2c0b9c192b04d
SR <saodat.riskiyeva@gmail.com>
2025-10-06 17:42:51 -0400

    graphql query

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const usersData = [
+  {
+    id: 1,
+    firstName: "Mike",
+    lastName: "Smith",
+  },
+  {
+    id: 2,
+    firstName: "Danny",
+    lastName: "Gold",
+  },
+];
+
+const addresses = [
+  {
+    userIs: 1,
+    houseNumber: 123,
+    street: "Main St",
+    zipCode: 12345,
+  },
+  {
+    userIs: 2,
+    houseNumber: 456,
+    street: "Second St",
+    zipCode: 67890,
+  },
+];
+
 const typeDefs = `#graphql
     type User {
         firstName: String!
@@ -12,18 +40,29 @@ const typeDefs = `#graphql
     
     type UserResponse {
         user: User
+        users: [User]
         address: Address
+        addresses: [Address]
+    }
+
+    type Query {
+        getSingleUser(id: ID!): UserResponse!
+        getAllUsers: UserResponse
     }
 `;
 
 const resolvers = {
-  Query: {},
+  Query: {
+    getSingleUser: (parent, args) => {
+      console.log(parent);
+      console.log(args);
+      const user = usersData.find((data) => data.id === args.id);
+      const address = addresses.find((data) => data.userId === args.id);
+      return {
+        user,
+        address,
+      };
+    },
+  },
   Mutation: {},
-  Subscriptions: {},
-  Address: {
-    houseNumber: (address) => parseInt(address.houseNumber),
-  },
-  User: {
-    lastName: (user) => console.log(user.lastName),
-  },
 };
